2f5e39ac62db976a71ec0422286f27d11e7a5fff
Howon Lee <hlee.howon@gmail.com>
2012-09-18 15:11:28 -0700

    add boulder spinning, modify ground

--- boulder.py
+++ boulder.py
@@ -2,6 +2,7 @@
 import vizact
 import vizproximity
 import viztask
+import vizinput
 import vizinfo
 #import vizsonic
 #from labtracker import *
@@ -9,10 +10,16 @@ import vizinfo
 class BoulderScene:
 	def __init__(self):
 		viz.go(viz.PROMPT)
+		#options
+		self.TAKEDATA = False
 		#init objects
-		self.ground = viz.add("ground.osgb")
+		self.ground = viz.add("ground_stone.osgb")
 		self.ground.setScale(5, 5, 5)
 		self.ground.addAction(vizact.moveTo([0, 0, -100], time=20))
+		self.start_time = viz.tick()
+		self.count = 0
+		self.data = ""
+		self.boulder_data = open('boulder_data.txt', 'a')
 		self.boulderSetup()
 		#hmd init
 		self.tracking = viz.get(viz.TRACKER)
@@ -21,7 +28,7 @@ class BoulderScene:
 		if (self.tracking):
 			self.Tracking = labTracker()
 			self.Tracking.setPosition(1,1,1)
-			
+
 	def boulderSetup(self):
 		self.boulder = viz.add("boulder.dae")
 		self.boulder.specular(0,0,0)
@@ -31,9 +38,19 @@ class BoulderScene:
 		#move = vizact.moveTo([0, 0, 100], time=20)
 		#spinmove = vizact.parallel(spin, move)
 		self.boulder.addAction(spin)
-		
+
 	def draw(self):
-		pass
-		
+		self.count += 1
+		if (self.count == 6 and self.TAKEDATA):
+			self.getdata()
+			self.count = 0
+
+	def getdata(self):
+		orientation = viz.MainView.getEuler()
+		position = viz.MainView.getPosition()
+		self.data = self.data + str(orientation) + '\t' + str(position) + '\n'
+		self.boulder_data.write(self.data)
+		self.boulder_data.flush()
+
 if __name__ == "__main__":
 	scene = BoulderScene()
